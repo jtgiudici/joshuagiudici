312a41c306b63bd97700fad7bf0c95504410c782
Joshua Giudici <jtgiudici@gmail.com>
2016-08-28 17:00:24 -0400

    new design - initial commit

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,73 +1,89 @@
 $(document).ready(function() {
-	$("header .icon.menu").click(function() {
+	$(".icon.menu").click(function() {
 		if ($(this).hasClass("open")) {
 			$(this).removeClass("open").addClass("close");
-			$("header .icon").addClass("dark");
-			$("#nav").css("height", "350px");
+			$("header nav").addClass("full");
 		}
 		else if ($(this).hasClass("close")) {
 			$(this).removeClass("close").addClass("open");
-			if ($(document).scrollTop() >= 350 && $(window).width() > 900) {
-				$("#nav").css("height", "50px");
-				$("header .icon").addClass("dark");
-			}
-			else if ($(document).scrollTop() >= 262.5 && $(window).width() < 900 && $(window).width() > 600) {
-				$("#nav").css("height", "50px");
-				$("header .icon").addClass("dark");
-			}
-			else if ($(document).scrollTop() >= 175 && $(window).width() < 600) {
-				$("#nav").css("height", "50px");
-				$("header .icon").addClass("dark");
-			}
-			else if ($(document).scrollTop() < 350) {
-				$("#nav").css("height", "0");
-				$("header .icon").removeClass("dark");
-			}
-			
-			if ($(document).scrollTop() < 350 && $(window).width() > 900) {
-				$("#nav").css("height", "0");
-				$("header .icon").removeClass("dark");
-			}
-			else if ($(document).scrollTop() < 262.5 && $(window).width() < 900 && $(window).width() > 600) {
-				$("#nav").css("height", "0");
-				$("header .icon").removeClass("dark");
-			}
-			else if ($(document).scrollTop() < 175 && $(window).width() < 600) {
-				$("#nav").css("height", "0");
-				$("header .icon").removeClass("dark");
-			}
+			$("header nav").removeClass("full");
 		}
 	});
-	$("#nav a").click(function() {
-		$("header .icon.menu").removeClass("close").addClass("open");
-		$("#nav").css("height", "50px");
-	});
-});
 
-$(document).scroll(function(){
-	if ($(document).scrollTop() >= 350 && $(window).width() > 900 && $("#nav").height() < 350) {
-		$("#nav").css("height", "50px");
-		$("header .icon").addClass("dark");
+	$("nav.controls a").click(function() {
+		if ($(this).hasClass("next") && !$(".desktop-screen .slide:visible").next(".slide").length == 0) {
+			$(".desktop-screen .slide:visible").hide().next(".slide").show();
+			$(".iphone-screen .slide:visible").hide().next(".slide").show();
+			number = $(".desktop-screen .slide:visible").index() + 1;
+			$("nav.dots .dot").removeClass("active");
+			$("nav.dots .dot:nth-child(" + number + ")").addClass("active");
+		}
+		else if ($(this).hasClass("next") && $(".desktop-screen .slide:visible").next(".slide").length == 0) {
+			$(".desktop-screen .slide:visible").hide();
+			$(".iphone-screen .slide:visible").hide()
+			$(".desktop-screen .slide:first-child").show();
+			$(".iphone-screen .slide:first-child").show();
+			number = $(".desktop-screen .slide:visible").index() + 1;
+			$("nav.dots .dot").removeClass("active");
+			$("nav.dots .dot:nth-child(" + number + ")").addClass("active");
+		}
+		else if ($(this).hasClass("prev") && !$(".desktop-screen .slide:visible").prev(".slide").length == 0) {
+			$(".desktop-screen .slide:visible").hide().prev(".slide").show();
+			$(".iphone-screen .slide:visible").hide().prev(".slide").show();
+			number = $(".desktop-screen .slide:visible").index() + 1;
+			$("nav.dots .dot").removeClass("active");
+			$("nav.dots .dot:nth-child(" + number + ")").addClass("active");
+		}
+		else if ($(this).hasClass("prev") && $(".desktop-screen .slide:visible").prev(".slide").length == 0) {
+			$(".desktop-screen .slide:visible").hide();
+			$(".iphone-screen .slide:visible").hide();
+			$(".desktop-screen .slide:last-child").show();
+			$(".iphone-screen .slide:last-child").show();
+			number = $(".desktop-screen .slide:visible").index() + 1;
+			$("nav.dots .dot").removeClass("active");
+			$("nav.dots .dot:nth-child(" + number + ")").addClass("active");
+		}
+	});
+
+	$("nav.dots .dot").click(function() {
+		number = $(this).index() + 1;
+		$("nav.dots .dot").removeClass("active");
+		$(this).addClass("active");
+		$(".desktop-screen .slide:visible").hide();
+		$(".iphone-screen .slide:visible").hide();
+		$(".desktop-screen .slide:nth-child(" + number + ")").show();
+		$(".iphone-screen .slide:nth-child(" + number + ")").show();
+	});
+
+	$('.desktop-screen.scroll').on('scroll', function () {
+		$('.iphone-screen.scroll').scrollTop($(this).scrollTop());
+	});
+	$('.iphone-screen.scroll').on('scroll', function () {
+		$('.desktop-screen.scroll').scrollTop($(this).scrollTop());
+	});
+
+	$.fn.scrollView = function () {
+		return this.each(function () {
+		$('html, body').animate({
+			scrollTop: $(this).offset().top
+		}, 1000);
+		});
 	}
-	else if ($(document).scrollTop() >= 262.5 && $(window).width() < 900 && $(window).width() > 600 && $("#nav").height() < 350) {
-		$("#nav").css("height", "50px");
-		$("header .icon").addClass("dark");
-	}
-	else if ($(document).scrollTop() >= 175 && $(window).width() < 600 && $("#nav").height() < 350) {
-		$("#nav").css("height", "50px");
-		$("header .icon").addClass("dark");
-	}
-	
-	if ($(document).scrollTop() < 350 && $(window).width() > 900 && $("#nav").height() < 350) {
-		$("#nav").css("height", "0");
-		$("header .icon").removeClass("dark");
-	}
-	else if ($(document).scrollTop() < 262.5 && $(window).width() < 900 && $(window).width() > 600 && $("#nav").height() < 350) {
-		$("#nav").css("height", "0");
-		$("header .icon").removeClass("dark");
-	}
-	else if ($(document).scrollTop() < 175 && $(window).width() < 600 && $("#nav").height() < 350) {
-		$("#nav").css("height", "0");
-		$("header .icon").removeClass("dark");
-	}
+	$("header nav a").click(function() {
+		if ($(this).hasClass("work")) {
+			$('section.work').scrollView();
+		}
+		else if ($(this).hasClass("contact")) {
+			$('section.contact').scrollView();
+		}
+
+	});
+	$("body.home header nav a").click(function() {
+		if ($(this).hasClass("about")) {
+			$('section.about').scrollView();
+		}
+		else if ($(this).hasClass("skills")) {
+			$('section.skills').scrollView();
+		}
+	});
 });
