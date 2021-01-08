e8972def84ba154beea3ff8e35fec6d261b56c1d
Joshua Giudici <jtgiudici@gmail.com>
2021-01-07 20:30:51 -0500

    Replace new site design

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,96 +1,45 @@
 $(document).ready(function() {
-	$(".icon.menu").click(function() {
-		if ($(this).hasClass("open")) {
-			$(this).removeClass("open").addClass("close");
-			$("header nav").addClass("full");
-		}
-		else if ($(this).hasClass("close")) {
-			$(this).removeClass("close").addClass("open");
-			$("header nav").removeClass("full");
-		}
-	});
-
-	$("nav.controls a").click(function() {
-		if ($(this).hasClass("next") && !$(".desktop-screen .slide:visible").next(".slide").length == 0) {
-			$(".desktop-screen .slide:visible").hide().next(".slide").show();
-			$(".iphone-screen .slide:visible").hide().next(".slide").show();
-			number = $(".desktop-screen .slide:visible").index() + 1;
-			$("nav.dots .dot").removeClass("active");
-			$("nav.dots .dot:nth-child(" + number + ")").addClass("active");
-		}
-		else if ($(this).hasClass("next") && $(".desktop-screen .slide:visible").next(".slide").length == 0) {
-			$(".desktop-screen .slide:visible").hide();
-			$(".iphone-screen .slide:visible").hide()
-			$(".desktop-screen .slide:first-child").show();
-			$(".iphone-screen .slide:first-child").show();
-			number = $(".desktop-screen .slide:visible").index() + 1;
-			$("nav.dots .dot").removeClass("active");
-			$("nav.dots .dot:nth-child(" + number + ")").addClass("active");
-		}
-		else if ($(this).hasClass("prev") && !$(".desktop-screen .slide:visible").prev(".slide").length == 0) {
-			$(".desktop-screen .slide:visible").hide().prev(".slide").show();
-			$(".iphone-screen .slide:visible").hide().prev(".slide").show();
-			number = $(".desktop-screen .slide:visible").index() + 1;
-			$("nav.dots .dot").removeClass("active");
-			$("nav.dots .dot:nth-child(" + number + ")").addClass("active");
-		}
-		else if ($(this).hasClass("prev") && $(".desktop-screen .slide:visible").prev(".slide").length == 0) {
-			$(".desktop-screen .slide:visible").hide();
-			$(".iphone-screen .slide:visible").hide();
-			$(".desktop-screen .slide:last-child").show();
-			$(".iphone-screen .slide:last-child").show();
-			number = $(".desktop-screen .slide:visible").index() + 1;
-			$("nav.dots .dot").removeClass("active");
-			$("nav.dots .dot:nth-child(" + number + ")").addClass("active");
-		}
-	});
-
-	$("nav.dots .dot").click(function() {
-		number = $(this).index() + 1;
-		$("nav.dots .dot").removeClass("active");
-		$(this).addClass("active");
-		$(".desktop-screen .slide:visible").hide();
-		$(".iphone-screen .slide:visible").hide();
-		$(".desktop-screen .slide:nth-child(" + number + ")").show();
-		$(".iphone-screen .slide:nth-child(" + number + ")").show();
-	});
-
-	$('.desktop-screen.scroll').on('scroll', function () {
-		$('.iphone-screen.scroll').scrollTop($(this).scrollTop());
-	});
-	$('.iphone-screen.scroll').on('scroll', function () {
-		$('.desktop-screen.scroll').scrollTop($(this).scrollTop());
-	});
-
-	$.fn.scrollView = function () {
-		return this.each(function () {
-		$('html, body').animate({
-			scrollTop: $(this).offset().top
+	$(".side-scroll nav a").click(function() {
+		var sectionName = $(this).attr("class");
+		$("html, body").animate({
+			scrollTop: $("section." + sectionName).position().top
 		}, 1000);
-		});
-	}
-	$("header nav a").click(function() {
-		if ($(this).hasClass("work")) {
-			$('section.work').scrollView();
-		}
-		else if ($(this).hasClass("contact")) {
-			$('section.contact').scrollView();
-		}
+		$(".side-scroll").addClass("animate");
+		setTimeout(function() {
+       $(".side-scroll").removeClass("animate");
+   }, 1000);
 
+		$(".side-scroll nav a").removeClass("active");
+		$(this).addClass("active");
 	});
-	$("body.home header nav a").click(function() {
-		if ($(this).hasClass("about")) {
-			$('section.about').scrollView();
-		}
-		else if ($(this).hasClass("skills")) {
-			$('section.skills').scrollView();
-		}
-	});
+	$(window).scroll(function() {
+    var position = $(window).scrollTop();
+		var menu = $(".side-scroll")
 
-	$(".fancybox").fancybox({
-		openEffect	: 'none',
-		closeEffect	: 'none',
-		nextEffect: "none",
-		prevEffect: "none"
+    $('section.project').each(function() {
+        var target = $(this).position().top - $(window).scrollTop();
+        var id = $(this).attr('id');
+
+				if ( target <= 0 && !menu.hasClass("animate") ) {
+					 $('.side-scroll nav a').removeClass('active');
+					 $('.side-scroll nav a.' + id + "").addClass('active');
+			 }
+    });
+
+		if (position >= 490) {
+			$(".side-scroll").addClass("scrolling");
+		} else if (position < 490) {
+			$(".side-scroll").removeClass("scrolling");
+		}
+		if (position >= 440) {
+			$("body.project-page header").addClass("scrolling");
+		} else if (position < 440) {
+			$("body.project-page header").removeClass("scrolling");
+		}
+		if (position >= 100) {
+			$("body.home header").addClass("scrolling");
+		} else if (position < 100) {
+			$("body.home header").removeClass("scrolling");
+		}
 	});
 });
